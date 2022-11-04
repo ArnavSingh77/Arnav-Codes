6cc7d941af40ef12b01b3ccf4ca502403337d8ee
ArnavSingh77 <72737311+ArnavSingh77@users.noreply.github.com>
2022-11-04 17:30:02 +0530

    Shortened the code

--- Python Codes/Activity 7- Square and Cube Calculator.py
+++ Python Codes/Activity 7- Square and Cube Calculator.py
@@ -1,20 +1,3 @@
-choice = input(
-    '''
-Please select the type of operation you want to perform with Arnav's Power Calculator:
-    2 for Square
-    3 for Cube
-    (More to be Added)
-    '''
-)
-
-x=int(input("Enter Number: "))
-sq= x**2
-cu=x**3
-
-
-#Multiplying
-if choice == '2':
-    print(x, "To the Power 2= ", sq)
-#Cubing
-elif choice == '3':
-    print(x, "To the Power 3= ", cu)
+base=int(input("Enter The Base: "))
+pow=int(input("Enter The Power: "))
+print(base, "To the Power of", pow, " = ", base**pow)
