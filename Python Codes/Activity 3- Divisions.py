29d2817caadfffce535b2a0d989332ea259a1dcb
ArnavSingh77 <72737311+ArnavSingh77@users.noreply.github.com>
2022-11-10 16:09:40 +0530

    Update Activity 3- Divisions.py

--- Python Codes/Activity 3- Divisions.py
+++ Python Codes/Activity 3- Divisions.py
@@ -4,11 +4,11 @@ x=int(input("Enter Your Marks: "))
 if x==100:
     print("Congrats! You are the topper!")
 elif x<100 and x>=75:
-    print("You achieived Division 1")
+    print("You achieved Division 1")
 elif x<75 and x>=50:
-    print("You achieived Division 2")
+    print("You achieved Division 2")
 elif x<50 and x>=30:
-    print("You achieived Division 3")
+    print("You achieved Division 3")
 elif x>100:
     print("You Cannot Fool Me! MARKS CANNOT BE GREATER THAN 100!")
 else:
