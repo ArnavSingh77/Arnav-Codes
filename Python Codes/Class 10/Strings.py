f55112c775a1f909d2f4883c39850fb665c70516
Arnav Singh <72737311+ArnavSingh77@users.noreply.github.com>
2023-08-17 17:03:23 +0530

    Update Strings.py

--- Python Codes/Class 10/Strings.py
+++ Python Codes/Class 10/Strings.py
@@ -1,1 +1,41 @@
+# Declaration of string
+text = "arNav is a GOod boY"
 
+# Converts all lowercase characters in a string into uppercase
+print("UPPER:", text.upper())
+
+# Converts the first character of the string to a capital (uppercase) letter
+print("CAPITALIZE:", text.capitalize())
+
+# Convert starting letter of each word to capital
+print("TITLE:", text.title())
+
+# Converts all uppercase characters in a string into lowercase
+print("LOWER:", text.lower())
+
+# Returns a concatenated String
+print("JOIN:", '-'.join(text))
+
+# Checks if all characters in the string are uppercase
+print("UPPER:",text.isupper())
+
+# Checks if all characters in the string are lowercase
+print("LOWER:",text.islower())
+
+# Returns “True” if the string is a title cased string
+print("TITLE:",text.istitle())
+
+# Returns “True” if all characters in the string are whitespace characters
+print("SPACE:",text.isspace())
+
+# It returns an integer which is the length of the string. 
+print("COUNT:", len(text))
+
+# Program to reverse a string
+print("REVERSE:",text[::-1])
+
+# Python Program to demonstrate String slicing
+print("SLICING:",text[3:12])
+
+# Splitting the string
+print("SPLIT:", text.split())
