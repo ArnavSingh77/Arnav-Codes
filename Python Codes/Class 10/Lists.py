68f805bedf4dfd41b03ad7dbba416c1587867a5c
Arnav Singh <72737311+ArnavSingh77@users.noreply.github.com>
2023-08-17 13:19:57 +0530

    Add files via upload

---
+++ Python Codes/Class 10/Lists.py
@@ -0,0 +1,31 @@
+a = ["arnav", "is", "a","good","boy"]
+
+#Append
+a.append(":)")
+print("Append: ", a)
+print("")
+
+#Insert
+a.insert(2,"not")
+print("Insert: ", a)
+print("")
+
+#Pop
+a.pop(2)
+print("Pop: ", a)
+print("")
+
+#Del
+del a[1]
+print("Del: ", a)
+print("")
+
+#Sort
+a.sort()
+print("Sort: ",a)
+print("")
+
+#Reverse
+a.reverse()
+print("Reversed: ",a)
+print("")
