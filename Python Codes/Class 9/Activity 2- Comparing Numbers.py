f33fc2eda4717c43a24d3c32fa46030cece19cfd
Arnav Singh <72737311+ArnavSingh77@users.noreply.github.com>
2024-01-17 16:24:46 +0530

    Added condition if all the numbers are same

--- Python Codes/Class 9/Activity 2- Comparing Numbers.py
+++ Python Codes/Class 9/Activity 2- Comparing Numbers.py
@@ -1,3 +1,5 @@
+# Write a program to find the maximum number out of the given three numbers.
+
 #Taking Input
 x=int(input("Enter the First Number to Compare: "))
 y=int(input("Enter the Second Number to Compare: "))
@@ -8,5 +10,7 @@ if x>y and x>z:
     print("First Number is the Greatest!")
 elif y>x and y>z:
     print("Second Number is the Greatest!")
-else:
+elif z>x and z>y:
     print("Third Number is the Greatest!")
+else:
+    print("All the entered numbers are the same!")
